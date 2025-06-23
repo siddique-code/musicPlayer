6671cd79aa662d77f65ad8fe25a24113c580aa36
Muhammad Siddique <msiddique88522@gmail.com>
2025-06-23 13:05:26 +0500

    corecting code in js

--- js/script.js
+++ js/script.js
@@ -127,7 +127,7 @@ async function displayAlbums() {
 
 async function main() {
   //Get the list of all the songs
-  await getSongs("songs/ncs");
+  await getSongs("songs/cs");
   playMusic(songs[0], true);
 
   //Display all the albums on the page
